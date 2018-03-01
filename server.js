dd732ae150c038440c12450e9577db1caabfd925
Eric Whitehead <ericwhitehead@Erics-Air.fios-router.home>
2018-02-28 22:14:03 -0500

    update 2

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.get('/', function(req,res) {
 	res.status(200).json({'message': 'got it'});
 });
