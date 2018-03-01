45010484f3be892ebfef01aa9b821e4e0b721cbe
Eric Whitehead <ericwhitehead@Erics-Air.fios-router.home>
2018-02-28 22:14:45 -0500

    update 2

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/', function(req,res) {
-	res.status(200).json({'message': 'got it'});
-});
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
